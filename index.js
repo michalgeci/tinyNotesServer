70a9c0be4a5d6b445d17c6d96c6c3f1b327ee22e
Michal Géci <michal.geci.1995@gmail.com>
2019-09-13 01:27:10 +0200

    Changed port and address

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ var db = low(adapter)
 
 // Setup server
 var app = express()
-var server = app.listen(3004, function(){
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var server = app.listen(server_port, server_ip_address, function(){
   console.log("Connected")
 })
 
